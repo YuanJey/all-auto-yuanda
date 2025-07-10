3b3f8f0b6a3d6d3c1eaf683a3c98948186878a7c
jayyuan <2218773049@qq.com>
2025-07-10 16:29:09 +0800

    zhuan

--- get_key.py
+++ get_key.py
@@ -10,6 +10,6 @@ def aes_encrypt(plaintext: str) -> str:
     ciphertext = cipher.encrypt(pad(plaintext.encode('utf-8'), BLOCK_SIZE))
     return base64.b64encode(ciphertext).decode('utf-8')
 if __name__ == "__main__":
-    dec="17600090001"
+    dec="13234560001"
     aes_encrypt(dec)
     print(aes_encrypt(dec))
